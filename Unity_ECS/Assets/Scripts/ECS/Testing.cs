4c798347913071e244c91a0a5c0376a29f321e0c
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2019-10-29 22:52:29 +0300

    Update gitignore and dummy entity archetype

--- Unity_ECS/Assets/Scripts/ECS/Testing.cs
+++ Unity_ECS/Assets/Scripts/ECS/Testing.cs
@@ -1,14 +1,26 @@
-﻿using UnityEngine;
+﻿using Unity.Collections;
+using UnityEngine;
 using Unity.Entities;
+using Unity.Transforms;
 
 public class Testing : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         var entityManager = World.Active.EntityManager;
-        var entity = entityManager.CreateEntity(typeof(LevelComponent));
+
+        var entityArchetype = entityManager.CreateArchetype(
+            typeof(LevelComponent),
+            typeof(Translation));
         
-        entityManager.SetComponentData(entity, new LevelComponent {Level = 10});
+        var entities = new NativeArray<Entity>(2000, Allocator.Temp);
+        entityManager.CreateEntity(entityArchetype, entities);
+
+        for (var i = 0; i < entities.Length; i++)
+        {
+            entityManager.SetComponentData(entities[i], new LevelComponent {Level = Random.Range(10, 200)});
+        }
+
+        entities.Dispose();
     }
 }
