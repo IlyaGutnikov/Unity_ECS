3ce71b0ede78c05f54dac9262dd332662a9fd0e5
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2019-10-29 23:07:33 +0300

    Mesh

--- Unity_ECS/Assets/Scripts/ECS/Testing.cs
+++ Unity_ECS/Assets/Scripts/ECS/Testing.cs
@@ -1,24 +1,35 @@
 ﻿using Unity.Collections;
 using UnityEngine;
 using Unity.Entities;
+using Unity.Rendering;
 using Unity.Transforms;
 
 public class Testing : MonoBehaviour
 {
+    [SerializeField] private Mesh _mesh;
+    [SerializeField] private Material _material;
+    
     void Start()
     {
         var entityManager = World.Active.EntityManager;
 
         var entityArchetype = entityManager.CreateArchetype(
             typeof(LevelComponent),
-            typeof(Translation));
+            typeof(Translation),
+            typeof(RenderMesh),
+            typeof(LocalToWorld));
         
-        var entities = new NativeArray<Entity>(2000, Allocator.Temp);
+        var entities = new NativeArray<Entity>(1, Allocator.Temp);
         entityManager.CreateEntity(entityArchetype, entities);
 
         for (var i = 0; i < entities.Length; i++)
         {
             entityManager.SetComponentData(entities[i], new LevelComponent {Level = Random.Range(10, 200)});
+            entityManager.SetSharedComponentData(entities[i], new RenderMesh
+            {
+                mesh = _mesh,
+                material = _material
+            });
         }
 
         entities.Dispose();
