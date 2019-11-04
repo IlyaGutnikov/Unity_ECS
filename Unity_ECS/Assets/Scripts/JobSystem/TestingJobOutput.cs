a542a0fb2b3f307b26693f8dbc774ba4728c1660
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2019-11-04 18:18:03 +0300

    Non-working example

---
+++ Unity_ECS/Assets/Scripts/JobSystem/TestingJobOutput.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Jobs;
+using UnityEngine;
+
+public class TestingJobOutput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        var job = new SimpleJob
+        {
+            A = 1,
+            B = 2
+        };
+
+        var jobHandle = job.Schedule();
+        jobHandle.Complete();
+        
+        Debug.Log(job.Result);
+    }
+    
+}
+
+public struct SimpleJob : IJob
+{
+    public int A;
+    public int B;
+    public int Result;
+    
+    public void Execute()
+    {
+        Result = A + B;
+    }
+}
