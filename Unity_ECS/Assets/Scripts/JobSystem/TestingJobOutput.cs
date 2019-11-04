24e3c7ba1a408605e283b9b3d1080897491e24a2
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2019-11-04 18:28:37 +0300

    Done lesson 6

--- Unity_ECS/Assets/Scripts/JobSystem/TestingJobOutput.cs
+++ Unity_ECS/Assets/Scripts/JobSystem/TestingJobOutput.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
 
@@ -8,16 +9,20 @@ public class TestingJobOutput : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
+        // В целях необходимости синхронизации данных между основным потоком и Job-ом
+        var result = new NativeArray<int>(1, Allocator.TempJob);
         var job = new SimpleJob
         {
             A = 1,
-            B = 2
+            B = 2,
+            Result = result
         };
 
         var jobHandle = job.Schedule();
         jobHandle.Complete();
         
-        Debug.Log(job.Result);
+        Debug.Log(job.Result[0]);
+        result.Dispose();
     }
     
 }
@@ -26,10 +31,10 @@ public struct SimpleJob : IJob
 {
     public int A;
     public int B;
-    public int Result;
+    public NativeArray<int> Result;
     
     public void Execute()
     {
-        Result = A + B;
+        Result[0] = A + B;
     }
 }
